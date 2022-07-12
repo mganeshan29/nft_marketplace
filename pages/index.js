9b2df26b5120735e58474bf629fd1add810d1db4
mganeshan29 <ganeshanmadesh36@outlook.com>
2022-07-12 22:05:45 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home = () => (
+  <div className="test-3xl font-bold underline">
+    <h1>hello World!</h1>
+  </div>
+);
+
+export default Home;
